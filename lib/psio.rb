6cffddc0c83267ca7a6d3902db02f02ed361b921
Marcus Ortiz <mportiz08@gmail.com>
2012-10-19 15:17:14 -0700

    Added some helper methods for retrieving processes owned by specific user.

--- lib/psio.rb
+++ lib/psio.rb
@@ -5,4 +5,11 @@ require 'psio/cpu'
 require 'psio/process'
 
 module Psio
+  def self.processes_for_user(user)
+    self.processes.select { |proc| proc.user == user }
+  end
+  
+  def self.processes_for_current_user
+    self.processes_for_user(ENV['USER'])
+  end
 end
